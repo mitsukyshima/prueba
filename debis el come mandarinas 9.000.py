8aef81f15356259d886e200449f518be6fa52df0
mitsuky <110051985+mitsukyshima@users.noreply.github.com>
2023-04-03 20:03:32 +0300

    Add files via upload

--- debis el come mandarinas 9.000.py
+++ debis el come mandarinas 9.000.py
@@ -1,2 +1,9 @@
-input=("inicio, holamundoyt")
+print("esto es un archivo de prueba meramente para experimentar su funcionamiento ")
+print ("Oh!, una personita nueva :D...no te habia visto por aqui  ")
+namee = input("Cual es tu nombre ?")
+print ("eh?" ) 
+print ("perdona no te entendi, lo puedes repetir? owo")
+name = input ("cual es tu nombre?")
+print(f"hola , {name} es un placer tenerte aqui :D")
+
 
