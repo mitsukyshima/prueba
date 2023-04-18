1103fc78fed1f8bfc1c3a3b2294aa752b78fa37e
mitsuky <110051985+mitsukyshima@users.noreply.github.com>
2023-04-18 22:08:25 +0300

    Add files via upload

--- debis el come mandarinas 9.000.py
+++ debis el come mandarinas 9.000.py
@@ -6,4 +6,19 @@ print ("perdona no te entendi, lo puedes repetir? owo")
 name = input ("cual es tu nombre?")
 print(f"hola , {name} es un placer tenerte aqui :D")
 
+print ("practica :")
 
+susu = input ("cual es tu nombre : ")
+matematicas = int(input (susu + " cual es tu calificacion en matematicas : "))
+quimica= int (input (susu + " cual es tu calificacion de quimica : "))
+biologia = int (input (susu + " cual es tu calificacion de biologia  : "))
+calificacion = (matematicas + quimica + biologia ) / 3 
+calificacion = int( calificacion )
+
+if calificacion >= 6 : 
+    print('felicidades :D ' +  susu + "  aprovaste UWU con un promedio de : " , calificacion )
+    print('felicidades :D ' +  susu + "  aprovaste UWU con un promedio de : " , round ( calificacion , 2))
+else: 
+    print(" :I ummm..." +  susu + " lo sentiento'reprobaste :/'  con un promedio de : "   , calificacion )
+    print(" :I ummm..." +  susu + " lo sentiento 'reprobaste :/'  con un promedio de : "   , round ( calificacion , 1 ))
+print ("fin :O . ")
